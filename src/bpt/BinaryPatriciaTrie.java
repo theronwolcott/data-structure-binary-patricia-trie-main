26e8142c0e96c8fae8cb7693581b428386df6fa0
theronwolcott <70415399+theronwolcott@users.noreply.github.com>
2024-03-28 21:30:46 -0400

    implemented everything but getLongest and delete

--- src/bpt/BinaryPatriciaTrie.java
+++ src/bpt/BinaryPatriciaTrie.java
@@ -2,21 +2,33 @@ package bpt;
 
 import bpt.UnimplementedMethodException;
 
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * <p>{@code BinaryPatriciaTrie} is a Patricia Trie over the binary alphabet &#123;	 0, 1 &#125;. By restricting themselves
- * to this small but terrifically useful alphabet, Binary Patricia Tries combine all the positive
- * aspects of Patricia Tries while shedding the storage cost typically associated with tries that
- * deal with huge alphabets.</p>
+ * <p>
+ * {@code BinaryPatriciaTrie} is a Patricia Trie over the binary alphabet &#123;
+ * 0, 1 &#125;. By restricting themselves
+ * to this small but terrifically useful alphabet, Binary Patricia Tries combine
+ * all the positive
+ * aspects of Patricia Tries while shedding the storage cost typically
+ * associated with tries that
+ * deal with huge alphabets.
+ * </p>
  *
  * @author YOUR NAME HERE!
  */
 public class BinaryPatriciaTrie {
 
-    /* We are giving you this class as an example of what your inner node might look like.
-     * If you would prefer to use a size-2 array or hold other things in your nodes, please feel free
-     * to do so. We can *guarantee* that a *correct* implementation exists with *exactly* this data
+    /*
+     * We are giving you this class as an example of what your inner node might look
+     * like.
+     * If you would prefer to use a size-2 array or hold other things in your nodes,
+     * please feel free
+     * to do so. We can *guarantee* that a *correct* implementation exists with
+     * *exactly* this data
      * stored in the nodes.
      */
     private static class TrieNode {
@@ -38,52 +50,147 @@ public class BinaryPatriciaTrie {
     }
 
     private TrieNode root;
+    private AtomicBoolean bool;
 
     /**
      * Simple constructor that will initialize the internals of {@code this}.
      */
     public BinaryPatriciaTrie() {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        root = new TrieNode("", false);
+        bool = new AtomicBoolean();
     }
 
     /**
      * Searches the trie for a given key.
      *
      * @param key The input {@link String} key.
-     * @return {@code true} if and only if key is in the trie, {@code false} otherwise.
+     * @return {@code true} if and only if key is in the trie, {@code false}
+     *         otherwise.
      */
     public boolean search(String key) {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        bool.set(false);
+        searchHelper(key, root);
+        return bool.get();
+    }
+
+    private void searchHelper(String key, TrieNode node) {
+        bool.set(false);
+        if (node == null) {
+            return;
+        }
+        if (node.str == "" && key.startsWith("0")) { // Root, go left
+            searchHelper(key, node.left);
+        } else if (node.str == "" && key.startsWith("1")) { // Root, go right
+            searchHelper(key, node.right);
+        } else if (key.equals(node.str) && node.isKey == true) { // Strings are equal, we found it
+            bool.set(true);
+            return;
+        } else if (key.startsWith(node.str) && key.substring(node.str.length()).startsWith("0")) {
+            // Node is a prefix of key, go left
+            searchHelper(key.substring(node.str.length()), node.left);
+        } else if (key.startsWith(node.str) && key.substring(node.str.length()).startsWith("1")) {
+            // Node is a prefix of key, go left
+            searchHelper(key.substring(node.str.length()), node.right);
+        }
     }
 
     /**
      * Inserts key into the trie.
      *
-     * @param key The input {@link String}  key.
-     * @return {@code true} if and only if the key was not already in the trie, {@code false} otherwise.
+     * @param key The input {@link String} key.
+     * @return {@code true} if and only if the key was not already in the trie,
+     *         {@code false} otherwise.
      */
     public boolean insert(String key) {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        bool.set(false);
+        root = insertHelper(key, root);
+        return bool.get();
     }
 
+    private TrieNode insertHelper(String key, TrieNode n) {
+        if (n == null) {
+            bool.set(true);
+            return new TrieNode(key, true);
+        }
+
+        if (key.equals(n.str) && n.isKey) { // if key is equal to str
+            return n;
+        } else if (key.equals(n.str) && !n.isKey) {
+            n.isKey = true;
+            bool.set(true);
+            return n;
+        }
+        if (key.startsWith(n.str) || n.str.equals("")) { // str is a prefix of key, chop off equal bit and recurse
+            if (key.substring(n.str.length()).startsWith("0")) {
+                n.left = insertHelper(key.substring(n.str.length()), n.left);
+            } else {
+                n.right = insertHelper(key.substring(n.str.length()), n.right);
+            }
+        } else if (n.str.startsWith(key)) { // key is a prefix of str, need a new node with equal part as child of n,
+                                            // existing gets updated
+            TrieNode newNode = new TrieNode(key, true); // the part that's equal
+            if (n.str.substring(key.length()).startsWith("0")) { // if we need to go left
+                newNode.left = n;
+
+            } else { // go right
+                newNode.right = n;
+            }
+            n.str = n.str.substring(key.length());
+            bool.set(true);
+            return newNode;
+        } else { // common prefix
+            int index = diffIndex(key, n.str);
+            String prefix = key.substring(0, index);
+            TrieNode newNode = new TrieNode(prefix, false);
+            n.str = n.str.substring(index);
+            if (key.substring(index).startsWith("0")) {
+                newNode.left = new TrieNode(key.substring(index), true);
+                newNode.right = n;
+            } else {
+                newNode.right = new TrieNode(key.substring(index), true);
+                newNode.left = n;
+            }
+            bool.set(true);
+            return newNode;
+        }
+        return n;
+    }
+
+    private int diffIndex(String key, String str) {
+        for (int i = 0; i < key.length(); i++) {
+            if (key.charAt(i) != str.charAt(i)) {
+                return i;
+            }
+        }
+        return -1; // never actually gets here
+    }
 
     /**
      * Deletes key from the trie.
      *
-     * @param key The {@link String}  key to be deleted.
-     * @return {@code true} if and only if key was contained by the trie before we attempted deletion, {@code false} otherwise.
+     * @param key The {@link String} key to be deleted.
+     * @return {@code true} if and only if key was contained by the trie before we
+     *         attempted deletion, {@code false} otherwise.
      */
     public boolean delete(String key) {
+        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+    }
+
+    private TrieNode deleteHelper(String key, TrieNode n) {
         throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
     }
 
     /**
      * Queries the trie for emptiness.
      *
-     * @return {@code true} if and only if {@link #getSize()} == 0, {@code false} otherwise.
+     * @return {@code true} if and only if {@link #getSize()} == 0, {@code false}
+     *         otherwise.
      */
     public boolean isEmpty() {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        if (getSize() == 0) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -92,62 +199,141 @@ public class BinaryPatriciaTrie {
      * @return The number of keys in the tree.
      */
     public int getSize() {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        return getSizeHelper(root);
+    }
+
+    private int getSizeHelper(TrieNode n) {
+        if (n == null) {
+            return 0;
+        }
+        if (n.isKey) {
+            return getSizeHelper(n.left) + getSizeHelper(n.right) + 1;
+        } else {
+            return getSizeHelper(n.left) + getSizeHelper(n.right) + 0;
+        }
     }
 
     /**
-     * <p>Performs an <i>inorder (symmetric) traversal</i> of the Binary Patricia Trie. Remember from lecture that inorder
-     * traversal in tries is NOT sorted traversal, unless all the stored keys have the same length. This
-     * is of course not required by your implementation, so you should make sure that in your tests you
-     * are not expecting this method to return keys in lexicographic order. We put this method in the
-     * interface because it helps us test your submission thoroughly and it helps you debug your code! </p>
+     * <p>
+     * Performs an <i>inorder (symmetric) traversal</i> of the Binary Patricia Trie.
+     * Remember from lecture that inorder
+     * traversal in tries is NOT sorted traversal, unless all the stored keys have
+     * the same length. This
+     * is of course not required by your implementation, so you should make sure
+     * that in your tests you
+     * are not expecting this method to return keys in lexicographic order. We put
+     * this method in the
+     * interface because it helps us test your submission thoroughly and it helps
+     * you debug your code!
+     * </p>
      *
-     * <p>We <b>neither require nor test </b> whether the {@link Iterator} returned by this method is fail-safe or fail-fast.
-     * This means that you  do <b>not</b> need to test for thrown {@link java.util.ConcurrentModificationException}s and we do
-     * <b>not</b> test your code for the possible occurrence of concurrent modifications.</p>
+     * <p>
+     * We <b>neither require nor test </b> whether the {@link Iterator} returned by
+     * this method is fail-safe or fail-fast.
+     * This means that you do <b>not</b> need to test for thrown
+     * {@link java.util.ConcurrentModificationException}s and we do
+     * <b>not</b> test your code for the possible occurrence of concurrent
+     * modifications.
+     * </p>
      *
-     * <p>We also assume that the {@link Iterator} is <em>immutable</em>, i,e we do <b>not</b> test for the behavior
-     * of {@link Iterator#remove()}. You can handle it any way you want for your own application, yet <b>we</b> will
-     * <b>not</b> test for it.</p>
+     * <p>
+     * We also assume that the {@link Iterator} is <em>immutable</em>, i,e we do
+     * <b>not</b> test for the behavior
+     * of {@link Iterator#remove()}. You can handle it any way you want for your own
+     * application, yet <b>we</b> will
+     * <b>not</b> test for it.
+     * </p>
      *
-     * @return An {@link Iterator} over the {@link String} keys stored in the trie, exposing the elements in <i>symmetric
-     * order</i>.
+     * @return An {@link Iterator} over the {@link String} keys stored in the trie,
+     *         exposing the elements in <i>symmetric
+     *         order</i>.
      */
     public Iterator<String> inorderTraversal() {
-        throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
+        ArrayList<String> list = new ArrayList<>();
+        fillArray(list, "", root);
+        int originalSize = list.size();
+        var listIterator = list.iterator();
+
+        Iterator<String> iterator = new Iterator<String>() {
+
+            @Override
+            public boolean hasNext() {
+                return listIterator.hasNext();
+            }
+
+            @Override
+            public String next() {
+                if (originalSize != list.size()) {
+                    throw new ConcurrentModificationException();
+                }
+                return listIterator.next();
+            }
+
+        };
+        return iterator;
+    }
+
+    private void fillArray(ArrayList<String> l, String key, TrieNode n) {
+        if (n == null) {
+            return;
+        }
+        String tempKey = key + n.str;
+        // go left
+        fillArray(l, tempKey, n.left);
+        // self
+        if (n.isKey) {
+            l.add(key);
+        }
+        // go right
+        fillArray(l, tempKey, n.right);
+
     }
 
     /**
      * Finds the longest {@link String} stored in the Binary Patricia Trie.
-     * @return <p>The longest {@link String} stored in this. If the trie is empty, the empty string &quot;&quot; should be
-     * returned. Careful: the empty string &quot;&quot;is <b>not</b> the same string as &quot; &quot;; the latter is a string
-     * consisting of a single <b>space character</b>! It is also <b>not the same as the</b> null <b>reference</b>!</p>
+     * 
+     * @return
+     *         <p>
+     *         The longest {@link String} stored in this. If the trie is empty, the
+     *         empty string &quot;&quot; should be
+     *         returned. Careful: the empty string &quot;&quot;is <b>not</b> the
+     *         same string as &quot; &quot;; the latter is a string
+     *         consisting of a single <b>space character</b>! It is also <b>not the
+     *         same as the</b> null <b>reference</b>!
+     *         </p>
      *
-     * <p>Ties should be broken in terms of <b>value</b> of the bit string. For example, if our trie contained
-     * only the binary strings 01 and 11, <b>11</b> would be the longest string. If our trie contained
-     * only 001 and 010, <b>010</b> would be the longest string.</p>
+     *         <p>
+     *         Ties should be broken in terms of <b>value</b> of the bit string. For
+     *         example, if our trie contained
+     *         only the binary strings 01 and 11, <b>11</b> would be the longest
+     *         string. If our trie contained
+     *         only 001 and 010, <b>010</b> would be the longest string.
+     *         </p>
      */
     public String getLongest() {
         throw new UnimplementedMethodException(); // ERASE THIS LINE AFTER YOU IMPLEMENT THE METHOD!
     }
 
     /**
-     * Makes sure that your trie doesn't have splitter nodes with a single child. In a Patricia trie, those nodes should
+     * Makes sure that your trie doesn't have splitter nodes with a single child. In
+     * a Patricia trie, those nodes should
      * be pruned.
-     * @return {@code true} iff all nodes in the trie either denote stored strings or split into two subtrees, {@code false} otherwise.
+     * 
+     * @return {@code true} iff all nodes in the trie either denote stored strings
+     *         or split into two subtrees, {@code false} otherwise.
      */
-    public boolean isJunkFree(){
+    public boolean isJunkFree() {
         return isEmpty() || (isJunkFree(root.left) && isJunkFree(root.right));
     }
 
-    private boolean isJunkFree(TrieNode n){
-        if(n == null){   // Null subtrees trivially junk-free
+    private boolean isJunkFree(TrieNode n) {
+        if (n == null) { // Null subtrees trivially junk-free
             return true;
         }
-        if(!n.isKey){   // Non-key nodes need to be strict splitter nodes
-            return ( (n.left != null) && (n.right != null) && isJunkFree(n.left) && isJunkFree(n.right) );
+        if (!n.isKey) { // Non-key nodes need to be strict splitter nodes
+            return ((n.left != null) && (n.right != null) && isJunkFree(n.left) && isJunkFree(n.right));
         } else {
-            return ( isJunkFree(n.left) && isJunkFree(n.right) ); // But key-containing nodes need not.
+            return (isJunkFree(n.left) && isJunkFree(n.right)); // But key-containing nodes need not.
         }
     }
 }
--- src/bpt/StudentTests.java
+++ src/bpt/StudentTests.java
@@ -1,4 +1,5 @@
 package bpt;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,29 +11,95 @@ import static org.junit.Assert.*;
  */
 public class StudentTests {
 
-
-    @Test public void testEmptyTrie() {
+    @Test
+    public void testEmptyTrie() {
         BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
 
-        assertTrue("Trie should be empty",trie.isEmpty());
+        assertTrue("Trie should be empty", trie.isEmpty());
         assertEquals("Trie size should be 0", 0, trie.getSize());
 
         assertFalse("No string inserted so search should fail", trie.search("0101"));
 
     }
 
-    @Test public void testFewInsertionsWithSearch() {
+    @Test
+    public void testInsertSearchBasic() {
         BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
 
-        assertTrue("String should be inserted successfully",trie.insert("00000"));
-        assertTrue("String should be inserted successfully",trie.insert("00011"));
-        assertFalse("Search should fail as string does not exist",trie.search("000"));
+        assertTrue(trie.insert("0"));
+        assertTrue(trie.insert("1"));
+
+        assertFalse(trie.insert("0"));
+        assertFalse(trie.insert("1"));
+
+        assertTrue(trie.insert("00"));
+        assertTrue(trie.insert("00001"));
+        assertTrue(trie.insert("11100"));
+        assertTrue(trie.insert("10"));
+
+        assertTrue(trie.insert("111"));
+        assertTrue(trie.insert("0000"));
+
+        assertTrue(trie.getSize() == 8);
+
+        assertTrue(trie.search("0"));
+        assertTrue(trie.search("1"));
+        assertTrue(trie.search("00"));
+        assertTrue(trie.search("00001"));
+        assertTrue(trie.search("11100"));
+        assertTrue(trie.search("10"));
+        assertTrue(trie.search("111"));
+        assertTrue(trie.search("0000"));
+
+        assertFalse(trie.insert("00"));
+        assertFalse(trie.insert("00001"));
+        assertFalse(trie.insert("11100"));
+        assertFalse(trie.insert("10"));
+        assertFalse(trie.insert("111"));
+        assertFalse(trie.insert("0000"));
 
     }
 
+    @Test
+    public void testInsertSearchBasic2() {
+        BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
 
-    //testing isEmpty function
-    @Test public void testFewInsertionsWithDeletion() {
+        assertTrue(trie.insert("000"));
+        assertTrue(trie.insert("0000"));
+        assertTrue(trie.insert("0001"));
+        assertTrue(trie.insert("001"));
+
+        assertTrue(trie.search("000"));
+        assertTrue(trie.search("0000"));
+        assertTrue(trie.search("0001"));
+        assertTrue(trie.search("001"));
+    }
+
+    @Test
+    public void testInsertSearchBasic3() {
+        BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
+
+        assertTrue(trie.insert("000"));
+        assertTrue(trie.insert("0000"));
+
+        assertTrue(trie.search("000"));
+        assertTrue(trie.search("0000"));
+
+    }
+
+    @Test
+    public void testFewInsertionsWithSearch() {
+        BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
+
+        assertTrue("String should be inserted successfully", trie.insert("00000"));
+        assertTrue("String should be inserted successfully", trie.insert("00011"));
+        assertFalse("Search should fail as string does not exist", trie.search("000"));
+
+    }
+
+    // testing isEmpty function
+    @Test
+    public void testFewInsertionsWithDeletion() {
         BinaryPatriciaTrie trie = new BinaryPatriciaTrie();
 
         trie.insert("000");
@@ -47,11 +114,13 @@ public class StudentTests {
         trie.delete("0"); // Failed deletion; should affect exactly nothing.
         assertEquals("After inserting five strings and requesting the deletion of one not in the trie, the trie " +
                 "should report five strings stored.", 5, trie.getSize());
-        assertTrue("After inserting five strings and requesting the deletion of one not in the trie, the trie had some junk in it!",
+        assertTrue(
+                "After inserting five strings and requesting the deletion of one not in the trie, the trie had some junk in it!",
                 trie.isJunkFree());
 
         trie.delete("011"); // Successful deletion
-        assertEquals("After inserting five strings and deleting one of them, the trie should report 4 strings.", 4, trie.getSize());
+        assertEquals("After inserting five strings and deleting one of them, the trie should report 4 strings.", 4,
+                trie.getSize());
         assertTrue("After inserting five strings and deleting one of them, the trie had some junk in it!",
                 trie.isJunkFree());
     }
